5a72f0c85a740881b14c47fbee0f7848717e9d57
Calvin Nguyen <calvin.nvqc@gmail.com>
2020-02-12 00:30:12 -0800

    cleanup

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
-
 export default combineReducers({
-  user: userReducer,
+  user: userReducer
 });
--- server/store/config.js
+++ server/store/config.js
@@ -5,7 +5,10 @@ export const config = {
   },
   env: {
     port: 8080,
-    mongoDBUri: 'mongodb://localhost/test',
+    mongoDBUri:
+      process.env.ENV === 'prod'
+        ? 'mongodb+srv://user:usertestingpassword@cluster0-yfpdy.mongodb.net/test?retryWrites=true&w=majority'
+        : 'mongodb://localhost/dpa',
     mongoHostName: process.env.ENV === 'prod' ? 'mongodbAtlas' : 'localhost',
   },
 };
